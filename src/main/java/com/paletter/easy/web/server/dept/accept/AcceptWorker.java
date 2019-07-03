9747c473dd9204a4d8cd9d0321218596914cac30
paletter <fb_blackstone@163.com>
2019-07-03 08:47:10 +0800

    1

--- src/main/java/com/paletter/easy/web/server/dept/accept/AcceptWorker.java
+++ src/main/java/com/paletter/easy/web/server/dept/accept/AcceptWorker.java
@@ -1,5 +1,6 @@
 package com.paletter.easy.web.server.dept.accept;
 
+import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 
 public class AcceptWorker implements Runnable {
@@ -7,7 +8,8 @@ public class AcceptWorker implements Runnable {
 	private Selector selector;
 	
 	public AcceptWorker() {
-		super();
+		selector = Selector.open();
+		server.register(selector, SelectionKey.OP_ACCEPT);
 	}
 
 	@Override
